c93953d289d0f912be973fe436414e981a32da39
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-09-30 15:33:20 +1000

    WIP

--- custom-badge-swift/ViewController.swift
+++ custom-badge-swift/ViewController.swift
@@ -17,26 +17,45 @@ class ViewController: UIViewController {
   }
   
   private func createBadges() {
+    let badge1 = CustomBadge.customBadgeWithString("2",
+      withStringColor: UIColor.whiteColor(),
+      withInsetColor: UIColor.redColor(),
+      withBadgeFrame: true,
+      withBadgeFrameColor: UIColor.whiteColor(),
+      withScale: 1.0,
+      withShining: true)
+    
     let badge2 = CustomBadge.customBadgeWithString("CustomBadge",
-      withStringColor: UIColor.blackColor(),
+      withStringColor: UIColor.yellowColor(),
       withInsetColor: UIColor.greenColor(),
       withBadgeFrame: true,
       withBadgeFrameColor: UIColor.yellowColor(),
       withScale: 1.5,
       withShining: true)
     
-    // Add Badges to View
+    // Add badges to view
     view.addSubview(badge2)
-  
-    addCenterXConstraints(badge2)
-    let constraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|-40-[badge2]-0@20-|",
-      options: nil, metrics: nil,
-      views: ["badge2":badge2 ])
+    view.addSubview(badge1)
     
+    // Position badges
+    
+    var constraints = NSLayoutConstraint.constraintsWithVisualFormat("[badge2][badge1]",
+      options: nil, metrics: nil,
+      views: ["badge1": badge1, "badge2": badge2])
     view.addConstraints(constraints)
     
-//    view.addConstraints(constraints)
+    constraints = NSLayoutConstraint.constraintsWithVisualFormat("V:[badge2][badge1]",
+      options: nil, metrics: nil,
+      views: ["badge1": badge1, "badge2": badge2])
+    view.addConstraints(constraints)
     
+  
+    addCenterXConstraints(badge2)
+    constraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|-40-[badge2]-0@20-|",
+      options: nil, metrics: nil,
+      views: ["badge2": badge2])
+    
+    view.addConstraints(constraints)
   }
   
   private func addCenterXConstraints(chilView: UIView) {
